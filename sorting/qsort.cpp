863c007dc1844356db318577f38a653dea9e096e
lastday154 <vothuong197@gmail.com>
2016-04-10 13:52:01 +0530

    quick sort

--- sorting/qsort.cpp
+++ sorting/qsort.cpp
@@ -15,36 +15,44 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-void partition(vector <int>  ar) {
+int partition(int ar[],int low,int high) {
    // Enter code for partitioning and printing here. 
-    int i = ar.size();
-    int p = ar[0];
-    for(int j= ar.size()-1;j>0;j--){
+    int i = high;
+    int p = ar[low];
+    for(int j= high-1;j>low;j--){
         if(p < ar[j]){
             i--;
             swap(ar[i],ar[j]);
         }
         
     }
-    swap(ar[i-1],ar[0]);
-    for(int i=0;i<ar.size();i++){
-            cout<<ar[i]<<" ";
-    }
+    swap(ar[i-1],ar[low]);
+    return i-1;
     
 }
+void quicksort(int ar[],int low,int high){
+  if(low<high){
+    int p = partition(ar,low,high);
+    quicksort(ar,low,p-1);
+    quicksort(ar,p+1,high);
+  }
+}
 int main(void) {
-   vector <int>  _ar;
+   
    int _ar_size;
    cin >> _ar_size;
-    
+    int _ar[_ar_size];
      for(int _ar_i=0; _ar_i<_ar_size; _ar_i++) {
         int _ar_tmp;
         cin >> _ar_tmp;
-        _ar.push_back(_ar_tmp); 
+        _ar[_ar_i] = _ar_tmp;
     }
 
-   partition(_ar);
-    
-   
+    partition(_ar,0,_ar_size);
+    quicksort(_ar,0,_ar_size);
+    for (int i = 0; i < _ar_size; ++i)
+    {
+      cout<< _ar[i]<<endl;
+    }
    return 0;
 }
